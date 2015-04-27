d1fd027c821a61c8e5a5b751081eae1f7117abfd
Stephen Rolfe Nielsen <rolfe.nielsen@gmail.com>
2015-04-27 12:39:30 -0600

    Mobile First CSS
    Changes made on master

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,10 +2,11 @@ $(function(){
     
     // Sticky Header adds CSS to make a header stick once it 
     // hits the top of the screen
+    $('.sticky-alias').height($('.sticky-header').outerHeight());
     var stickyHeaderTop = $('.sticky-header').offset().top;
     
     $(window).scroll(function(){
-        if( $(window).scrollTop() > stickyHeaderTop ) {
+        if( $(window).scrollTop() >= stickyHeaderTop ) {
             $('.sticky-alias').css('display', 'block');
             $('.sticky-header').addClass('header-fixed');
         }
@@ -17,5 +18,18 @@ $(function(){
 
     // On mobile, the menu will toggle open and closed via
     // a touch method rather than on hover
+    $(document).on('click touchstart','#menu-toggle',function(){
+        var wrapperFooter = $('.page-wrap, .site-footer'),
+            menu = $('.slide-holder');
+
+        if(wrapperFooter.hasClass('menu-open')) {
+            wrapperFooter.removeClass('menu-open');
+            menu.removeClass('active');
+        }
+        else {
+            wrapperFooter.addClass('menu-open');
+            menu.addClass('active');
+        }
+    });
 
 });
