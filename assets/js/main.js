6e073ecef22d700c161d51c76408e0a0faa1645f
Stephen Rolfe Nielsen <rolfe.nielsen@gmail.com>
2015-04-28 10:18:58 -0600

    Revert ba51e89..f7cfd41
    This rolls back to commit ba51e89084c9c1dbfabb24d9a660dd4e34b01eba.

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,24 +2,17 @@ $(function(){
     
     // Sticky Header adds CSS to make a header stick once it 
     // hits the top of the screen
-    var sticky = $('.sticky-header'),
-        alias = $('.sticky-alias'),
-        stickyHeaderTop = sticky.offset().top;
-
-    if(stickyHeaderTop === 0) {
-        alias.height(sticky.outerHeight());
-        sticky.addClass('header-fixed');
-    }
+    $('.sticky-alias').height($('.sticky-header').outerHeight());
+    var stickyHeaderTop = $('.sticky-header').offset().top;
     
     $(window).scroll(function(){
         if( $(window).scrollTop() >= stickyHeaderTop ) {
-            alias.height(sticky.outerHeight());
-            alias.css('display', 'block');
-            sticky.addClass('header-fixed');
+            $('.sticky-alias').css('display', 'block');
+            $('.sticky-header').addClass('header-fixed');
         }
         else {
-            alias.css('display', 'none');
-            sticky.removeClass('header-fixed');
+            $('.sticky-alias').css('display', 'none');
+            $('.sticky-header').removeClass('header-fixed');
         }
     });
 
